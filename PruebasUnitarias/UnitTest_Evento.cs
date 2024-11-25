d80d9b47343f1b083701f53adb473ff6e79b46df
MauricioHerCab <ingenierosenior.desarrollo5@interrapidisimo.com>
2024-11-25 11:04:00 -0500

    pruebas unitarias

---
+++ PruebasUnitarias/UnitTest_Evento.cs
@@ -0,0 +1,103 @@
+﻿namespace PruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest_Evento
+    {
+        [TestMethod]
+        public void ConsultarEventosDisponibles_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void ConsultarEventosDisponibles_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void CrearEvento_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void CrearEvento_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void ModificarEvento_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void ModificarEvento_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void EliminarEvento_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void EliminarEvento_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void InscripcionAEvento_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void InscripcionAEvento_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void DimisionDeEvento_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void DimisionDeEvento_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+    }
+}
---
+++ PruebasUnitarias/UnitTest_Usuario.cs
@@ -0,0 +1,55 @@
+﻿namespace PruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest_Usuario
+    {
+        [TestMethod]
+        public void RegistrarUsuario_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void RegistrarUsuario_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void LoginUsuario_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void LoginUsuario_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void ValidarToken_Ok()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void ValidarToken_Fail()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+    }
+}
